ea7927014b9c95509848a9c8cd5aa16dac093c55
Yulia Fig <fihlevska@gmail.com>
2026-05-29 00:22:59 +0200

    hw6-next

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -17,7 +17,7 @@ export default function NoteDetailsClient() {
     return(<div className={css.container}>
 	<div className={css.item}>
 	  <div className={css.header}>
-	    <h2>Note title</h2>
+	    <h2>{data?.title}</h2>
 	  </div>
       <p className={css.tag}>{data?.tag}</p>
 	  <p className={css.content}>{data?.content}
